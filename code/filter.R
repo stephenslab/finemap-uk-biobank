33ea71d4d0af986b386f54c66cbc93a3b7152e08
zouyuxin <yuxin.zou.stat@gmail.com>
2019-05-21 22:32:31 -0500

    add code to filter samples

---
+++ code/filter.R
@@ -0,0 +1,35 @@
+## Filter data
+library(dplyr)
+
+dat.file = '/gpfs/data/stephens-lab/finemap-uk-biobank/height.csv'
+out.file = '/gpfs/data/stephens-lab/finemap-uk-biobank/height.filter.csv'
+### Read data
+dat = read.csv(dat.file, header = T)
+
+### Remove individual with sex mismatched between self-reported sex and genetic sex
+### This removes 310 individuals
+dat = dat %>% filter(sex == sex_genetic)
+
+### Remove individual with high missing rate
+### This removes 198 individuals
+dat = dat %>% filter(missingness < 0.05)
+
+### Remove outliers defined by UK BioBank when we have the data
+
+### Remove related individuals
+dat = dat %>% filter(is.na(relatedness_genetic0))
+
+### Remove individual with abnormal height
+### remove individuals with height departing 10 standard deviations from their gender mean
+dat.m = dat %>% filter(sex == 1) %>% filter(height < (mean(height) + 10*sd(height)) & height > (mean(height) - 10*sd(height)))
+dat.f = dat %>% filter(sex == 0) %>% filter(height < (mean(height) + 10*sd(height)) & height > (mean(height) - 10*sd(height)))
+dat = rbind(dat.m, dat.f)
+
+# SUMMARIZE DATA
+# --------------
+# Double-check that everything looks okay.
+summary(dat)
+
+# WRITE DATA TO FILE
+# ------------------
+write.csv(dat,out.file,row.names = FALSE,quote = FALSE)
