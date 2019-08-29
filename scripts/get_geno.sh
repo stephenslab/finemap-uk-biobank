665fd47fbc7c559dce6814033d7c74ffee1b7ea1
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-08-29 15:44:55 -0500

    Added additional filtering options to plink call in get_geno.sh to remove multiallelic SNPs.

--- scripts/get_geno.sh
+++ scripts/get_geno.sh
@@ -2,7 +2,7 @@
 
 # TO DO: Explain here what this script does, and how to use it.
 #
-#   qsub -I -l walltime=012:00:00 -l nodes=1:ppn=8 -l mem=40gb
+#   qsub -I -l walltime=24:00:00 -l nodes=1:ppn=8 -l mem=40gb
 #
 # TO DO: How long (roughly) does this script take to complete?
 #
@@ -17,7 +17,7 @@ LDSTORE_EXEC=ldstore
 BGEN_FILE=/gpfs/data/pierce-lab/uk-biobank-genotypes/ukb_imp_chr3_v3.bgen
 GENO_SAMPLE_FILE=/gpfs/data/xhe-lab/uk-biobank/data/genotypes/ukb27386_imp_v3_s487324.sample
 SNPID_FILE=../data/region-variants-ZBTB38.txt.gz
-OUT_FILE=ukb-imp-chr3-height-ZBTB38
+PLINK_OUTPUT=ukb-imp-chr3-height-ZBTB38
 CHR=3
 
 # EXTRACT SAMPLE IDs
@@ -25,16 +25,20 @@ CHR=3
 # Extract the ids of the phenotype samples from the first column of
 # the CSV file.
 echo "Extracting sample ids from phenotype file."
-zcat $PHENO_FILE | tail -n +2 | cut -d "," -f 1 > $PHENO_SAMPLE_FILE
+zcat $PHENO_FILE | tail -n +2 | cut -d "," -f 1 > temp
+paste -d " "  temp temp > ids
 
 # EXTRACT GENOTYPES
 # -----------------
-# TO DO: Explain here what this plink command does in more detail.
-echo "To do: Explain what this plink command does."
+# Create a PLINK binary file (.pgen) containing the genotypes for the
+# selected samples and selected SNPs. We additionally filter out any
+# SNPs that are not bi-allelic, or have minor allele frequencies less
+# than 1%.
+echo "Extracting genotype data for selected SNPs to PLINK binary file."
 $PLINK2_EXEC --sample $GENO_SAMPLE_FILE --bgen $BGEN_FILE --chr $CHR \
   --keep $PHENO_SAMPLE_FILE --extract $SNPID_FILE --maf 0.01 minor \
-  --snps-only --make-pgen --threads 8 --memory 38000 \
-  --out $OUT_FILE
+  --snps-only --max-alleles 2 --rm-dup exclude-all --make-pgen \
+  --threads 8 --memory 38000 --out $PLINK_OUTPUT
  
 # plink2 --pfile height.chr3.140800000.141800000.0.01 --threads 8 --export A \
 #        --out height.chr3.140800000.141800000.0.01
