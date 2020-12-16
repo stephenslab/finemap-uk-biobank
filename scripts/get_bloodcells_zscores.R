fbb603d18caf47cc5e1a52bf9d64d49412b1f269
zouyuxin <yuxin.zou.stat@gmail.com>
2020-12-16 12:45:53 -0600

    update zscores

--- scripts/get_bloodcells_zscores.R
+++ scripts/get_bloodcells_zscores.R
@@ -4,12 +4,13 @@ chr = as.numeric(arg[1])
 start = as.numeric(arg[2])
 end = as.numeric(arg[3])
 
-library(data.table)
-library(dplyr)
+suppressMessages(library(data.table))
+suppressMessages(library(dplyr))
+suppressMessages(library(Matrix))
 
-gwas_dir = '/gpfs/data/stephens-lab/finemap-uk-biobank/data/raw/BloodCells/gwas/'
-genotype_dir = '/gpfs/data/stephens-lab/finemap-uk-biobank/data/raw/BloodCells/regions_genotype/'
-output_dir = '/gpfs/data/stephens-lab/finemap-uk-biobank/data/raw/BloodCells/regions_zscores/'
+gwas_dir = '/gpfs/data/stephens-lab/finemap-uk-biobank/data/raw/BloodCells/gwas_maf001_info6/'
+genotype_dir = '/gpfs/data/stephens-lab/finemap-uk-biobank/data/raw/BloodCells/regions_genotype_maf001_info6/'
+output_dir = '/gpfs/data/stephens-lab/finemap-uk-biobank/data/raw/BloodCells/regions_zscores_maf001_info6/'
 
 pheno_names = c("WBC_count", "RBC_count", "Haemoglobin", "MCV", "RDW", "Platelet_count",
                 "Plateletcrit", "PDW", "Lymphocyte_perc", "Monocyte_perc",
@@ -18,7 +19,7 @@ pheno_names = c("WBC_count", "RBC_count", "Haemoglobin", "MCV", "RDW", "Platelet
 
 zscores = c()
 for(trait in pheno_names){
-  gwas = fread(paste0('/gpfs/data/stephens-lab/finemap-uk-biobank/data/raw/BloodCells/gwas/bloodcells_gwas_chr', chr, '.', trait, '.glm.linear'))
+  gwas = fread(paste0(gwas_dir,'bloodcells_gwas_chr', chr, '.', trait, '.glm.linear'))
   colnames(gwas)[1] = 'CHR'
   dt = gwas %>% filter(CHR == chr, POS >= start, POS <= end) %>% select(CHR, POS, ID, REF, ALT, T_STAT) %>% 
     rename(!!trait := T_STAT)
@@ -36,14 +37,15 @@ geno.file = paste0(genotype_dir, 'bloodcells_chr',
                    chr, '.', start, '.', end, '.raw.gz')
 
 cat("Reading genotype data.\n")
-geno <- fread(geno.file,sep = "\t",header = TRUE,stringsAsFactors = FALSE)
+geno <- suppressMessages(fread(geno.file,sep = "\t",header = TRUE,stringsAsFactors = FALSE))
 class(geno) <- "data.frame"
 # Extract the genotypes.
-X <- as.matrix(geno[-(1:6)])
+X <- as(as.matrix(geno[-(1:6)]),'dgCMatrix')
+# X <- as.matrix(geno[-(1:6)])
 
 pheno.file <- "/gpfs/data/stephens-lab/finemap-uk-biobank/data/raw/BloodCells/bloodcells.pheno.txt"
 cat("Reading phenotype data.\n")
-pheno        <- fread(pheno.file)
+pheno        <- suppressMessages(fread(pheno.file))
 class(pheno) <- "data.frame"
 
 ind = fread(paste0(genotype_dir, 'bloodcells_chr', 
@@ -55,15 +57,13 @@ Y = pheno %>% select(-FID, -IID) %>% as.matrix
 
 # centering
 Y = t(t(Y) - colMeans(Y))
-# Center X
+XtY = as.matrix(Matrix::crossprod(X, Y))
+
 X.cm = colMeans(X)
-X = t(X) - X.cm
-xtxdiag = rowSums(X^2)
+xtxdiag = colSums(X^2) - nrow(X) * X.cm^2
 
-XtY = X %*% Y
-
-maf <- read.delim(paste0(genotype_dir, 'bloodcells_chr', 
-                         chr, '.', start, '.', end, '.afreq'))
+maf <- suppressMessages(read.delim(paste0(genotype_dir, 'bloodcells_chr', 
+                         chr, '.', start, '.', end, '.afreq')))
 maf = maf %>% mutate(maf = pmin(ALT_FREQS, 1-ALT_FREQS)) %>% select(ID, maf)
 pos = inner_join(pos, maf, by='ID')
 
